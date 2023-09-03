5a003690ba00e8e69966e27c6d85de159dd0d4db
Sihuan Newrise <sihuannewrise@gmail.com>
2023-09-03 14:17:13 +0300

    admin registered

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from orders.models import DeliveryAddress, OrderProducts, Orders
 
-admin.site.register(DeliveryAddress, OrderProducts, Orders)
-
+admin.site.register(DeliveryAddress)
+admin.site.register(OrderProducts)
+admin.site.register(Orders)
