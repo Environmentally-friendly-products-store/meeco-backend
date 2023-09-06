87b7b3e472fa06faa87402e3782108096c1afcdf
sihuannewrise <sihuannewrise@gmail.com>
2023-09-06 12:31:17 +0300

    links to other models updated

--- orders/models.py
+++ orders/models.py
@@ -1,16 +1,33 @@
+from django.conf import settings
 from django.db import models
 from orders import appvars as VARS
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
+
+
+class DeliveryAddress(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="deliveryAddress",
+    )
+    country = models.CharField(max_length=VARS.DEL_ADDR_COUNTRY_ML)
+    city = models.CharField(max_length=VARS.DEL_ADDR_CITY_ML)
+    street = models.CharField(max_length=VARS.DEL_ADDR_STREET_ML)
+    house = models.CharField(max_length=VARS.DEL_ADDR_HOUSE_ML)
+    apartment = models.CharField(max_length=VARS.DEL_ADDR_APARTMENT_ML)
 
 
 class Orders(models.Model):
     article_number = models.CharField(max_length=VARS.ORDERS_ARTICLE_ML)
     customer = models.ForeignKey(
-        "User",
+        User,
         on_delete=models.CASCADE,
         related_name="orders",
     )
     address = models.ForeignKey(
-        "DeliveryAddress",
+        DeliveryAddress,
         on_delete=models.SET_NULL,
     )
     created_at = models.DateTimeField(auto_now_add=True)
@@ -26,21 +43,8 @@ class OrderProducts(models.Model):
         related_name="orderProducts",
     )
     product_id = models.ForeignKey(
-        "Products",
+        Product,
         on_delete=models.SET_NULL,
     )
     amount = models.FloatField()
     purchase_price = models.FloatField()
-
-
-class DeliveryAddress(models.Model):
-    owner = models.ForeignKey(
-        "User",
-        on_delete=models.CASCADE,
-        related_name="deliveryAddress",
-    )
-    country = models.CharField(max_length=VARS.DEL_ADDR_COUNTRY_ML)
-    city = models.CharField(max_length=VARS.DEL_ADDR_CITY_ML)
-    street = models.CharField(max_length=VARS.DEL_ADDR_STREET_ML)
-    house = models.CharField(max_length=VARS.DEL_ADDR_HOUSE_ML)
-    apartment = models.CharField(max_length=VARS.DEL_ADDR_APARTMENT_ML)
