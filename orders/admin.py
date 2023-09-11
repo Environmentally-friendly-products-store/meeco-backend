24361a55580aa14845b832a4649d1e583dae0dea
sihuannewrise <sihuannewrise@gmail.com>
2023-09-11 11:02:59 +0300

    admin interface changed

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from orders.models import DeliveryAddress, OrderProduct, Order
 
-admin.site.register(DeliveryAddress)
-
 
 @admin.register(DeliveryAddress)
 class DeliveryAddressAdmin(admin.ModelAdmin):
