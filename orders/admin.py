c0691668ddf99b456496f8bd767af331fbb7e409
sihuannewrise <sihuannewrise@gmail.com>
2023-09-11 10:56:43 +0300

    admin interface changed

--- orders/admin.py
+++ orders/admin.py
@@ -2,5 +2,42 @@ from django.contrib import admin
 from orders.models import DeliveryAddress, OrderProduct, Order
 
 admin.site.register(DeliveryAddress)
-admin.site.register(OrderProduct)
-admin.site.register(Order)
+
+
+@admin.register(DeliveryAddress)
+class DeliveryAddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "owner",
+        "country",
+        "city",
+        "street",
+        "house",
+        "apartment",
+    )
+
+
+@admin.register(OrderProduct)
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order_id",
+        "product_id",
+        "amount",
+        "street",
+        "purchase_price",
+    )
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "article_number",
+        "customer",
+        "address",
+        "created_at",
+        "price_total",
+        "status",
+        "comment",
+    )
