f3824cbf165b5c6835cbfb7512d768f36e653af4
Дмитрий Патин <patin.work@gmail.com>
2023-09-12 12:58:57 +0300

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py collectstatic --clear --noinput
 
 cp -r /app/collected_static/. /backend_static/static/
 
-gunicorn meecco.wsgi:application --bind 0.0.0.0:8000
+gunicorn meeco.wsgi:application --bind 0.0.0.0:8000
