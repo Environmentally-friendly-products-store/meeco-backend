5d71a57eaac6a0c7dd4090c109da3b46bebacf0a
sihuannewrise <sihuannewrise@gmail.com>
2023-09-13 11:27:58 +0300

    model fields mods

--- orders/models.py
+++ orders/models.py
@@ -13,11 +13,19 @@ class DeliveryAddress(models.Model):
         on_delete=models.CASCADE,
         related_name="deliveryAddress",
     )
-    country = models.CharField(max_length=VARS.DEL_ADDR_COUNTRY_ML)
+    country = models.CharField(
+        max_length=VARS.DEL_ADDR_COUNTRY_ML,
+        blank=True,
+        null=True,
+    )
     city = models.CharField(max_length=VARS.DEL_ADDR_CITY_ML)
     street = models.CharField(max_length=VARS.DEL_ADDR_STREET_ML)
     house = models.CharField(max_length=VARS.DEL_ADDR_HOUSE_ML)
-    apartment = models.CharField(max_length=VARS.DEL_ADDR_APARTMENT_ML)
+    apartment = models.CharField(
+        max_length=VARS.DEL_ADDR_APARTMENT_ML,
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         ordering = ("owner",)
@@ -25,24 +33,40 @@ class DeliveryAddress(models.Model):
         verbose_name_plural = "адреса доставки"
 
     def __str__(self):
-        return self.country
+        return self.street
 
 
 class Order(models.Model):
-    article_number = models.CharField(max_length=VARS.ORDER_ARTICLE_ML)
+    article_number = models.CharField(
+        max_length=VARS.ORDER_ARTICLE_ML,
+        blank=True,
+        null=True,
+    )
     customer = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name="orders",
     )
-    address = models.ForeignKey(DeliveryAddress, on_delete=models.SET_NULL, null=True)
+    address = models.ForeignKey(
+        DeliveryAddress,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     price_total = models.FloatField(
         blank=True,
         null=True,
     )
-    status = models.CharField(max_length=VARS.ORDER_STATUS_ML)
-    comment = models.TextField()
+    status = models.CharField(
+        max_length=VARS.ORDER_STATUS_ML,
+        blank=True,
+        null=True,
+    )
+    comment = models.TextField(
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         ordering = ("customer",)
@@ -55,9 +79,16 @@ class Order(models.Model):
 
 class OrderProduct(models.Model):
     order_id = models.ForeignKey(
-        Order, on_delete=models.SET_NULL, related_name="orderProducts", null=True
+        Order,
+        on_delete=models.SET_NULL,
+        related_name="orderProducts",
+        null=True,
     )
-    product_id = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    product_id = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
     amount = models.FloatField()
     purchase_price = models.FloatField()
 
