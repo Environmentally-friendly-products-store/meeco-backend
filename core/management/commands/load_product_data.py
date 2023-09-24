9c29bb4e4dec45b6271219ca89e9d095eb3cec3a
Игорь Горбенко <gorbenkoigorek@mail.ru>
2023-09-24 23:54:32 +0300

    Minor fix in load_product_data function

--- core/management/commands/load_product_data.py
+++ core/management/commands/load_product_data.py
@@ -5,28 +5,28 @@ from products.models import Category, Product
 
 
 class Command(BaseCommand):
-    help = "Download data from folder to db"
+    help = "Download data from tsv file to db"
 
     def handle(self, *args, **options):
         self.import_products()
 
-    def import_products(self, file="./data/test_prod_data.csv"):
-        with open(file) as f:
+    def import_products(self, file="./data/test_prod_data.tsv"):
+        with open(file, encoding='utf-8') as f:
             data = f.readlines()
         for item in data:
-            datalist = item.split(sep=",")
-            if Category.objects.filter(name=datalist[4].strip()).exists():
-                category = Category.objects.get(name=datalist[4].strip())
+            datalist = item.split(sep="	")
+            if Category.objects.filter(name=datalist[4].strip()[:30]).exists():
+                category = Category.objects.get(name=datalist[4].strip()[:30])
             else:
                 category = Category.objects.create(
-                    name=datalist[4].strip(),
-                    description=datalist[4].strip(),
-                    slug=cyrillic_slug_maker(datalist[4].strip()),
+                    name=datalist[4].strip()[:30],
+                    description=datalist[4].strip()[:255],
+                    slug=cyrillic_slug_maker(datalist[4].strip()[:30]),
                 )
             Product.objects.get_or_create(
-                name=datalist[0],
-                description=datalist[1],
-                brand=datalist[2],
+                name=datalist[0][:30],
+                description=datalist[1][:255],
+                brand=datalist[2][:30],
                 price_per_unit=datalist[3],
-                category=category.id,
+                category=category,
             )
