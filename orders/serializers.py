264bc34175cde9931ea3d3d0c6f378a1f80a0151
Sihuan Newrise <sihuannewrise@gmail.com>
2023-09-24 19:21:18 +0300

    serializer mods

--- orders/serializers.py
+++ orders/serializers.py
@@ -6,40 +6,41 @@ from rest_framework import serializers
 
 from orders.models import Order, OrderProduct
 
-# from products.serializers import ShortProductSerializer
-
-
-class ItemTotalCalc(serializers.Field):
-    """Поле для вычисления стоимости позиции в заказе."""
-
-    def to_representation(self, value):
-        return value
-
-    def to_internal_value(self, obj):
-        return round(obj.amount * obj.purchase_price, 2)
-
 
 class OrderProductSerializer(serializers.ModelSerializer):
-    # product_name = ShortProductSerializer(source="product_id")
+    product_name = serializers.StringRelatedField(source="product_id")
     purchase_price = serializers.FloatField()
-    item_total = ItemTotalCalc()
+    item_total = serializers.SerializerMethodField()
 
     class Meta:
         model = OrderProduct
         fields = (
             "id",
-            "product_id",
+            "product_name",
             "amount",
             "purchase_price",
             "item_total",
         )
         read_only_fields = ("item_total",)
 
+    def get_item_total(self, obj):
+        return round(obj.amount * obj.purchase_price, 2)
+
+    def create(self, validated_data):
+        item_total = validated_data.get("amount") * validated_data.get("purchase_price")
+        validated_data.update(
+            {
+                "item_total": item_total,
+            }
+        )
+        return super().create(validated_data)
+
 
 class OrderSerializer(serializers.ModelSerializer):
     article_number = serializers.CharField(
+        allow_null=True,
         required=False,
-        default="",
+        # default="",
     )
     customer = UserSerializer(
         default=serializers.CurrentUserDefault(),
@@ -72,29 +73,53 @@ class OrderSerializer(serializers.ModelSerializer):
             "price_total",
         )
 
-    def to_representation(self, instance):
-        result = super().to_representation(instance)
-        return OrderedDict(
-            [(key, result[key]) for key in result if result[key] not in [None, ""]]
-        )
-
     def get_price_total(self, obj):
         return obj.products.aggregate(Sum("item_total"))["item_total__sum"]
 
-    # def create(self, validated_data):
-    #     if "products" not in self.initial_data:
-    #         order = Order.objects.create(**validated_data)
-    #         return order
-    #     products = validated_data.pop("products")
-    #     order = Order.objects.create(**validated_data)
-    #     for product in products:
-    #         current_product, status = OrderProduct.objects.get_or_create(
-    #             **product,
-    #         )
-    #         OrderProduct.objects.create(
-    #             order_id=order,
-    #             product_id=current_product.id,
-    #             amount=current_product.amount,
-    #             purchase_price=current_product.purchase_price,
-    #         )
-    #     return order
+    # def get_customer(self, obj):
+    #     return self.context["request"].user in obj.customer_id.all()
+
+    def to_representation(self, instance):
+        result = super().to_representation(instance)
+        return OrderedDict(
+            [
+                (key, result[key])
+                for key in result
+                if result[key]
+                not in [
+                    None,
+                    "",
+                ]
+            ]
+        )
+
+    def create(self, validated_data):
+        if "products" not in self.initial_data:
+            order = Order.objects.create(**validated_data)
+            return order
+        products = validated_data.pop("products")
+        order = Order.objects.create(**validated_data)
+        for product in products:
+            OrderProduct.objects.create(order_id=order, **product)
+        return order
+
+    def update(self, instance, validated_data):
+        instance.article_number = validated_data.get(
+            "article_number", instance.article_number
+        )
+        instance.contact_phone_number = validated_data.get(
+            "contact_phone_number", instance.contact_phone_number
+        )
+        instance.address = validated_data.get("address", instance.address)
+        instance.status = validated_data.get("status", instance.status)
+        instance.comment = validated_data.get("comment", instance.comment)
+        if "products" in validated_data:
+            products = validated_data.pop("products")
+            lst = []
+            for product in products:
+                current_product, status = OrderProduct.objects.get_or_create(**product)
+                lst.append(current_product)
+            instance.products.set(lst)
+
+        instance.save()
+        return instance
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from api.pagination import Pagination
 from orders.models import Order
@@ -6,12 +8,27 @@ from orders.serializers import OrderSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
+    queryset = Order.objects.prefetch_related("customer").all()
     serializer_class = OrderSerializer
     pagination_class = Pagination
+    permission_classes = [permissions.IsAuthenticated]
+
+    @action(
+        detail=False,
+        methods=["get"],
+    )
+    def current_user_orders(self, request):
+        queryset = self.get_queryset().filter(user=request.user)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"request": self.request})
+        return context
 
     def perform_create(self, serializer):
         serializer.save(customer=self.request.user)
 
-    def perform_update(self, serializer):
-        serializer.save(customer=self.request.user)
+    # def perform_update(self, serializer):
+    #     serializer.save(customer=self.request.user)
