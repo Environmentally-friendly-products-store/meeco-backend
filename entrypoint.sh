5c047ff3ffa3a7971c80b01fe2baba4d35e4bcb8
Elena Lambert <elena_lambert@yahoo.com>
2023-10-17 12:43:24 +0300

    8080 port

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ python manage.py migrate
 
 python manage.py collectstatic --clear --noinput
 
-gunicorn meeco.wsgi:application --bind 0.0.0.0:8000
+gunicorn meeco.wsgi:application --bind 0.0.0.0:8080
