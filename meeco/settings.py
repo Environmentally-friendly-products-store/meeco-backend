bc15837dc392e9174acdab561ce78fbcc3e6f7f1
ElenaL1 <115463400+ElenaL1@users.noreply.github.com>
2023-10-17 14:31:26 +0300

    Update settings.py

--- meeco/settings.py
+++ meeco/settings.py
@@ -15,9 +15,9 @@ DEBUG = os.getenv("DEBUG", default=False)
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default="*").split(",")
 
 # CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS")
-CSRF_TRUSTED_ORIGINS=["http://www.ecome.acceleratorpracticum.ru",
+CSRF_TRUSTED_ORIGINS=["https://ecome.acceleratorpracticum.ru",
                       "https://www.ecome.acceleratorpracticum.ru",
-                      "http://www.test-ecome.acceleratorpracticum.ru",
+                      "https://test-ecome.acceleratorpracticum.ru",
                       "https://www.test-ecome.acceleratorpracticum.ru"]
 
 CORS_ORIGIN_ALLOW_ALL = True
