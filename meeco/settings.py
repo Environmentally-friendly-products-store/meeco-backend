b51715abb2660760041a9015888c953571a263c2
sihuannewrise <sihuannewrise@gmail.com>
2023-10-28 19:43:15 +0300

    mail_api

--- meeco/settings.py
+++ meeco/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "meeco.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "core" / "mail_api"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
