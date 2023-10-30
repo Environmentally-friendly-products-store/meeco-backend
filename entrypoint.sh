1f4d873c238e32ad06150e58436432d9403c5adc
Elena Lambert <elena_lambert@yahoo.com>
2023-10-30 18:39:46 +0300

    Correct port 8080 for 8000 for test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ python manage.py migrate
 
 python manage.py collectstatic --clear --noinput
 
-gunicorn meeco.wsgi:application --bind 0.0.0.0:8080
+gunicorn meeco.wsgi:application --bind 0.0.0.0:8000
