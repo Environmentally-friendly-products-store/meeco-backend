087c86bb296572f31d99a51f6f8c65b363873337
ElenaL1 <115463400+ElenaL1@users.noreply.github.com>
2023-12-06 18:32:51 +0300

    Update settings.py

--- meeco/settings.py
+++ meeco/settings.py
@@ -16,8 +16,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default="*").split(",")
 
 # CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS")
 CSRF_TRUSTED_ORIGINS = [
-    "https://ecome.acceleratorpracticum.ru",
-    "https://test-ecome.acceleratorpracticum.ru",
+    "https://ecomestore.sytes.net",
     "http://ecomestore.sytes.net"
 ]
 
